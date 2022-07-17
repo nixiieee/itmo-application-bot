3e75d3052b74176d459b792bc0d71ecb93805a6c
Kseniia <kseniia_z@MacBook-Pro-Kseniia.local>
2022-07-17 13:13:46 +0300

    make it better

--- script.py
+++ script.py
@@ -17,9 +17,8 @@ def get_html(url):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     driver.get(url)
 
